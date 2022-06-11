a356710ddf6642b20524d1995d1958d17f6e5391
Pete <petefoth@e.email>
2022-06-11 10:36:27 +0100

    Move the EXTENDROM vars to caller
    Change-Id: Iba9dd432a5da44f0e40e3f688fd7250b459b4457

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,10 @@
 ########### extendrom section ###########
-#export ENABLE_EXTENDROM=true
+#export ENABLE_EXTENDROM=false
+#export RELEASE_TYPE='4microg-PLUS-E-UNOFFICIAL'
 
 
 ########### l4m plus e custom build ###########
-export EXTENDROM_PACKAGES="AuroraStore AuroraServices BlissLauncher ICSx5 DAVx5 NextCloud  NextCloudNotes OpenTasks K9-Mail-latest Fennec QKSMS OpenCamera noLOSSnap noLOSEmail noLOSMessaging noLOSJelly Bromite bromite_additional_repos.xml"
+#export EXTENDROM_PACKAGES="MicrogGmsCore F-Droid F-DroidPrivilegedExtension_pb  AuroraStore AuroraServices NominatimBackend MozillaNlpBackend GsfProxy FakeStore DroidGuard BlissLauncher ICSx5 DAVx5 NextCloud  NextCloudNotes OpenTasks K9-Mail-latest Fennec QKSMS OpenCamera noLOSSnap noLOSEmail noLOSMessaging noLOSJelly Bromite bromite_additional_repos.xml"
 
 $PWD/vendor/extendrom/get_prebuilts.sh
 
