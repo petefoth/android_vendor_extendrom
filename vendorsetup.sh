c0444410b13d3904ddd98b8db312a83c8496e922
Pete Fotheringham <petefoth@e.email>
2022-08-21 09:29:36 +0100

    Update vendorsetup.sh
    We'll use version from `unofficial_l4m_builds` project instead

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,17 +1,1 @@
-########### extendrom section ###########
-#export ENABLE_EXTENDROM=false
-#export RELEASE_TYPE='4microg-PLUS-E-UNOFFICIAL'
-
-
-########### l4m plus e custom build ###########
-#export EXTENDROM_PACKAGES="MicrogGmsCore F-Droid F-DroidPrivilegedExtension_pb  AuroraStore AuroraServices NominatimBackend MozillaNlpBackend GsfProxy FakeStore DroidGuard BlissLauncher ICSx5 DAVx5 NextCloud  NextCloudNotes OpenTasks K9-Mail-latest Fennec QKSMS OpenCamera noLOSSnap noLOSEmail noLOSMessaging noLOSJelly Bromite bromite_additional_repos.xml"
-
-$PWD/vendor/extendrom/get_prebuilts.sh
-
-if grep -q 'inherit-product-if-exists, vendor/extendrom/config/common.mk' $PWD/vendor/lineage/config/common.mk ; then
-  echo "already there"
-else
-  echo "adding"
-  echo "\$(call inherit-product-if-exists, vendor/extendrom/config/common.mk)" >> $PWD/vendor/lineage/config/common.mk
-fi
-
+We will overwrite this using file from `unofficial_l4m_builds` project
